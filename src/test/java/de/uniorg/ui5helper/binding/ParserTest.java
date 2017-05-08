b2a8d9d5bab23b27b2abc3f19bc1b3296771867a
Mark Schmale <m.schmale@uniorg.de>
2017-05-08 17:14:41 +0200

    retry travis build

--- src/test/java/de/uniorg/ui5helper/binding/ParserTest.java
+++ src/test/java/de/uniorg/ui5helper/binding/ParserTest.java
@@ -9,10 +9,10 @@ public class ParserTest extends ParsingTestCase {
 
     public void testSimpleBinding() { doTest(true); }
     public void testSimpleBindingWithoutModel() { doTest(true); }
+    public void testI18nBinding() { doTest(true); }
+    public void testMixedSimple() { doTest(true); }
 
     @Override
-    protected String getTestDataPath() {
-        return "src/test/resources/parser";
-    }
+    protected String getTestDataPath() { return "src/test/resources/parser"; }
 
 }
