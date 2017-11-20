b3572ef9b1b84ed33233539a26ea74943ccfc9b3
Mark Schmale <m.schmale@uniorg.de>
2017-11-20 09:37:16 +0100

    fixed tests

--- src/test/java/de/uniorg/ui5helper/folding/XmlViewControllerNameFolderTest.java
+++ src/test/java/de/uniorg/ui5helper/folding/XmlViewControllerNameFolderTest.java
@@ -1,6 +1,9 @@
 package de.uniorg.ui5helper.folding;
 
 import com.intellij.testFramework.fixtures.LightPlatformCodeInsightFixtureTestCase;
+import de.uniorg.ui5helper.Features;
+import de.uniorg.ui5helper.ProjectComponent;
+import de.uniorg.ui5helper.settings.Settings;
 
 import java.io.File;
 
@@ -12,6 +15,25 @@ public class XmlViewControllerNameFolderTest extends LightPlatformCodeInsightFix
 
     public void testControllerNameFolding() {
         myFixture.configureByFiles();
+        Settings.getInstance(myFixture.getProject()).pluginEnabled = true;
+        Settings.getInstance(myFixture.getProject()).foldControllerName = true;
+        assertTrue(ProjectComponent.isEnabled(myFixture.getProject(), Features.XML_COLLAPSE_CONTROLLER_NAME));
         myFixture.testFolding(this.getTestDataPath() + "/ControllerNameFolding.view.xml");
+    }
+
+    public void testControllerNameFoldingDisabled() {
+        myFixture.configureByFiles();
+        Settings.getInstance(myFixture.getProject()).pluginEnabled = true;
+        Settings.getInstance(myFixture.getProject()).foldControllerName = false;
+        assertFalse(ProjectComponent.isEnabled(myFixture.getProject(), Features.XML_COLLAPSE_CONTROLLER_NAME));
+        myFixture.testFolding(this.getTestDataPath() + "/ControllerNameFoldingDisabled.view.xml");
+    }
+
+    public void testControllerNameFoldingPluginDisabled() {
+        myFixture.configureByFiles();
+        Settings.getInstance(myFixture.getProject()).pluginEnabled = false;
+        Settings.getInstance(myFixture.getProject()).foldControllerName = true;
+        assertFalse(ProjectComponent.isEnabled(myFixture.getProject(), Features.XML_COLLAPSE_CONTROLLER_NAME));
+        myFixture.testFolding(this.getTestDataPath() + "/ControllerNameFoldingDisabled.view.xml");
     }
 }
