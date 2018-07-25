a038851ab83be63970afcefe7f0396e4186ca87e
Mark Schmale <m.schmale@uniorg.de>
2018-07-25 13:50:18 +0200

    Provide declaration information for tags

--- src/main/java/de/uniorg/ui5helper/codeInsight/xmlview/TagProvider.java
+++ src/main/java/de/uniorg/ui5helper/codeInsight/xmlview/TagProvider.java
@@ -1,17 +1,31 @@
 package de.uniorg.ui5helper.codeInsight.xmlview;
 
+import com.intellij.lang.javascript.JavaScriptFileType;
+import com.intellij.lang.javascript.psi.JSCallExpression;
 import com.intellij.openapi.project.Project;
+import com.intellij.openapi.vfs.VirtualFile;
+import com.intellij.psi.PsiElement;
+import com.intellij.psi.PsiManager;
 import com.intellij.psi.impl.source.xml.XmlElementDescriptorProvider;
+import com.intellij.psi.search.GlobalSearchScope;
 import com.intellij.psi.xml.XmlTag;
+import com.intellij.util.indexing.FileBasedIndexImpl;
 import com.intellij.xml.XmlElementDescriptor;
 import de.uniorg.ui5helper.ProjectComponent;
 import de.uniorg.ui5helper.codeInsight.xmlview.tags.ControlTag;
+import de.uniorg.ui5helper.framework.JSTreeUtil;
+import de.uniorg.ui5helper.index.JavascriptClassIndexer;
 import de.uniorg.ui5helper.ui.mvc.XmlViewUtil;
 import de.uniorg.ui5helper.ui5.ApiIndex;
 import de.uniorg.ui5helper.ui5.ApiSymbol;
 import de.uniorg.ui5helper.ui5.ClassDocumentation;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
 
 import static de.uniorg.ui5helper.ProjectComponent.isEnabled;
 
@@ -36,9 +50,31 @@ public class TagProvider implements XmlElementDescriptorProvider {
             return null;
         }
 
-        return new ControlTag(classDoc, context);
+        PsiElement decl = this.getDeclaration(classDoc, context.getProject());
+
+        return new ControlTag(classDoc, context, decl);
     }
 
+    private PsiElement getDeclaration(ClassDocumentation classDoc, Project project) {
+        Collection<VirtualFile> fileCollection = FileBasedIndexImpl.getInstance().getContainingFiles(JavascriptClassIndexer.KEY, classDoc.getName(), GlobalSearchScope.getScopeRestrictedByFileTypes(GlobalSearchScope.allScope(project), JavaScriptFileType.INSTANCE));
+
+        PsiManager psiManager = PsiManager.getInstance(project);
+
+        List<JSCallExpression> classDefinitions = fileCollection.stream()
+                .map(psiManager::findFile)
+                .filter(Objects::nonNull)
+                .filter(psiFile -> psiFile.getFileType().equals(JavaScriptFileType.INSTANCE))
+                .map(JSTreeUtil::getDefineCall)
+                .filter(Objects::nonNull)
+                .map(JSTreeUtil::findClassDeclaration)
+                .collect(Collectors.toList());
+
+        if (!classDefinitions.isEmpty()) {
+            return classDefinitions.get(0);
+        }
+
+        return null;
+    }
 
     private ClassDocumentation getClassDoc(@NotNull XmlTag tag) {
         ApiIndex index = this.getApiIndex(tag.getProject());
